5a9a14ad912b2358468fdbafbc58ba11c9b1ccbf
Stefan Bilharz <stefan@sbsoftware.de>
2024-06-03 20:26:17 +0200

    CRT-9 Fix click call

--- src/stimulus_controllers/boolean_flip_controller.cr
+++ src/stimulus_controllers/boolean_flip_controller.cr
@@ -4,7 +4,7 @@ class BooleanFlipController < Stimulus::Controller
   targets :submitButton, :formEl
 
   action :flip do
-    this.submitButtonTarget.click
+    this.submitButtonTarget.click._call
   end
 
   # TODO: Remove as soon as `crumble-stimulus` supports selectors

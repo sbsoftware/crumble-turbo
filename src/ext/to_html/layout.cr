6947541037e1a3b3a3559c116f7e1c47edb5eb58
Stefan Bilharz <stefan@sbsoftware.de>
2025-02-01 22:16:05 +0100

    CRT-27 Auto-load turbo.js in ToHtml::Layout

--- src/ext/to_html/layout.cr
+++ src/ext/to_html/layout.cr
@@ -2,5 +2,7 @@ require "to_html"
 require "../../stimulus_controllers/model_template_refresh_controller"
 
 class ToHtml::Layout
+  add_to_head ExternalScript.new("https://unpkg.com/@hotwired/turbo@8.0.4/dist/turbo.es2017-umd.js")
+
   body_attributes Crumble::Turbo::ModelTemplateRefreshController
 end

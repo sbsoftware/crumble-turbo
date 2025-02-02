c194adcc722a107ece779f6da8fa12ca1a6b1981
Stefan Bilharz <stefan@sbsoftware.de>
2025-02-02 10:50:48 +0100

    CRT-28 Add custom styles to base layout

--- src/ext/to_html/layout.cr
+++ src/ext/to_html/layout.cr
@@ -3,6 +3,8 @@ require "../../stimulus_controllers/model_template_refresh_controller"
 
 class ToHtml::Layout
   add_to_head ExternalScript.new("https://unpkg.com/@hotwired/turbo@8.0.4/dist/turbo.es2017-umd.js")
+  add_to_head Crumble::Turbo::Action::FormTemplate::Style
+  add_to_head Orma::ModelAction::GenericModelActionTemplate::Style
 
   body_attributes Crumble::Turbo::ModelTemplateRefreshController
 end

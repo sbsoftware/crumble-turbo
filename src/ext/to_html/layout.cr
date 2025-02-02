2414ee4da1be5daf7e14cb488f97101b3e0fbeb7
Stefan Bilharz <stefan@sbsoftware.de>
2025-02-02 10:51:51 +0100

    CRT-28 Use full class name

--- src/ext/to_html/layout.cr
+++ src/ext/to_html/layout.cr
@@ -2,7 +2,7 @@ require "to_html"
 require "../../stimulus_controllers/model_template_refresh_controller"
 
 class ToHtml::Layout
-  add_to_head ExternalScript.new("https://unpkg.com/@hotwired/turbo@8.0.4/dist/turbo.es2017-umd.js")
+  add_to_head ToHtml::ExternalScript.new("https://unpkg.com/@hotwired/turbo@8.0.4/dist/turbo.es2017-umd.js")
   add_to_head Crumble::Turbo::Action::FormTemplate::Style
   add_to_head Orma::ModelAction::GenericModelActionTemplate::Style
 

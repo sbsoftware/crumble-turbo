98a112a1f7a6b0b8e16d1f81934d92abe2e5c63f
Stefan Bilharz <stefan@sbsoftware.de>
2025-02-18 21:05:10 +0100

    CRT-29 Update shards

--- src/ext/to_html/layout.cr
+++ src/ext/to_html/layout.cr
@@ -2,9 +2,9 @@ require "to_html"
 require "../../stimulus_controllers/model_template_refresh_controller"
 
 class ToHtml::Layout
-  add_to_head ToHtml::ExternalScript.new("https://unpkg.com/@hotwired/turbo@8.0.4/dist/turbo.es2017-umd.js")
-  add_to_head Crumble::Turbo::Action::FormTemplate::Style
-  add_to_head Orma::ModelAction::GenericModelActionTemplate::Style
+  append_to_head ToHtml::ExternalScript.new("https://unpkg.com/@hotwired/turbo@8.0.4/dist/turbo.es2017-umd.js")
+  append_to_head Crumble::Turbo::Action::FormTemplate::Style
+  append_to_head Orma::ModelAction::GenericModelActionTemplate::Style
 
   body_attributes Crumble::Turbo::ModelTemplateRefreshController
 end

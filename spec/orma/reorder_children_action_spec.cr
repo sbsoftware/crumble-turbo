a9627b49f459a74ac1405913473f66ef9be6e7fe
Stefan Bilharz <stefan@sbsoftware.de>
2025-09-08 21:38:02 +0200

    CRT-35 Fix ReorderChildrenAction

--- spec/orma/reorder_children_action_spec.cr
+++ spec/orma/reorder_children_action_spec.cr
@@ -43,21 +43,23 @@ module ReorderChildrenActionSpec
       child_2 = Child.create(parent_id: parent.id, name: "Two")
 
       expected = <<-HTML.squish
-      <div data-controller="reorder-children-action--template--drag" data-action="dragstart->reorder-children-action--template--drag#dragstart drag->reorder-children-action--template--drag#drag dragover->reorder-children-action--template--drag#dragover dragenter->reorder-children-action--template--drag#dragenter drop->reorder-children-action--template--drag#drop dragend->reorder-children-action--template--drag#dragend">
-        <form class="reorder-children-action--template--hidden" action="/a/reorder_children_action_spec/parent/1/sort_children" method="POST">
-          <input data-reorder-children-action--template--drag-target="subjectId" type="hidden" name="subject_id">
-          <input data-reorder-children-action--template--drag-target="targetId" type="hidden" name="target_id">
-          <input data-reorder-children-action--template--drag-target="submit" type="submit" name="submit" value="submit">
-        </form>
-        <div>
-          <div data-reorder-children-action-subject-id="1" draggable="true">
-            <div data-model-template-id="ReorderChildrenActionSpec::Child#1-default_view" data-crumble--turbo--model-template-refresh-target="modelTemplate">
-              <div>One</div>
+      <div id="reorder-children-action-spec--parent--sort-children-action--template--id">
+        <div data-controller="reorder-children-action-spec--parent--sort-children-action--template--drag" data-action="dragstart->reorder-children-action-spec--parent--sort-children-action--template--drag#dragstart drag->reorder-children-action-spec--parent--sort-children-action--template--drag#drag dragover->reorder-children-action-spec--parent--sort-children-action--template--drag#dragover dragenter->reorder-children-action-spec--parent--sort-children-action--template--drag#dragenter drop->reorder-children-action-spec--parent--sort-children-action--template--drag#drop dragend->reorder-children-action-spec--parent--sort-children-action--template--drag#dragend">
+          <form class="crumble--turbo--action-form--hidden" action="/a/reorder_children_action_spec/parent/1/sort_children" method="POST">
+            <input data-reorder-children-action-spec--parent--sort-children-action--template--drag-target="subjectId" type="hidden" name="subject_id">
+            <input data-reorder-children-action-spec--parent--sort-children-action--template--drag-target="targetId" type="hidden" name="target_id">
+            <input data-reorder-children-action-spec--parent--sort-children-action--template--drag-target="submit" type="submit" name="submit" value="submit">
+          </form>
+          <div>
+            <div data-reorder-children-action-subject-id="1" draggable="true">
+              <div data-model-template-id="ReorderChildrenActionSpec::Child#1-default_view" data-crumble--turbo--model-template-refresh-target="modelTemplate">
+                <div>One</div>
+              </div>
             </div>
-          </div>
-          <div data-reorder-children-action-subject-id="2" draggable="true">
-            <div data-model-template-id="ReorderChildrenActionSpec::Child#2-default_view" data-crumble--turbo--model-template-refresh-target="modelTemplate">
-              <div>Two</div>
+            <div data-reorder-children-action-subject-id="2" draggable="true">
+              <div data-model-template-id="ReorderChildrenActionSpec::Child#2-default_view" data-crumble--turbo--model-template-refresh-target="modelTemplate">
+                <div>Two</div>
+              </div>
             </div>
           </div>
         </div>
--- src/orma/reorder_children_action.cr
+++ src/orma/reorder_children_action.cr
@@ -43,76 +43,78 @@ abstract class ReorderChildrenAction < Orma::ModelAction
   end
 
   macro inherited
-    view do
-      delegate :association, :child_view, to: action
+    macro finished
+      view do
+        delegate :association, :child_view, to: action
 
-      stimulus_controller DragController do
-        targets :subject_id, :target_id, :submit
+        stimulus_controller DragController do
+          targets :subject_id, :target_id, :submit
 
-        action :dragstart do |event|
-          event.dataTransfer.setData("text/plain", event.target.dataset.reorderChildrenActionSubjectId)
-          event.dataTransfer.allowedEffect = "move" # not sure if this is needed?
-        end
+          action :dragstart do |event|
+            event.dataTransfer.setData("text/plain", event.target.dataset.reorderChildrenActionSubjectId)
+            event.dataTransfer.allowedEffect = "move" # not sure if this is needed?
+          end
 
-        action :drag do |event|
-          if event.clientY > (window.outerHeight - 120) && window.scrollY < document.body.clientHeight
-            window.scrollTo({"top" => window.scrollY + 10})
-          elsif event.clientY < 120 && window.scrollY > 0
-            window.scrollTo({"top" => window.scrollY - 10})
+          action :drag do |event|
+            if event.clientY > (window.outerHeight - 120) && window.scrollY < document.body.clientHeight
+              window.scrollTo({"top" => window.scrollY + 10})
+            elsif event.clientY < 120 && window.scrollY > 0
+              window.scrollTo({"top" => window.scrollY - 10})
+            end
+          end
+
+          action :dragover do |event|
+            event.preventDefault._call
+            return true
+          end
+
+          action :dragenter do |event|
+            event.preventDefault._call
+          end
+
+          action :drop do |event|
+            subject_id = event.dataTransfer.getData("text/plain")
+            draggedElement = this.element.querySelector("[data-reorder-children-action-subject-id='" + subject_id + "']")
+            target = event.target.closest("[draggable=\"true\"]")
+            target_id = target.dataset.reorderChildrenActionSubjectId
+            positionComparison = target.compareDocumentPosition(draggedElement)
+
+            this.subjectIdTarget.value = subject_id
+            this.targetIdTarget.value = target_id
+
+            if positionComparison & 4
+              target.insertAdjacentElement("beforebegin", draggedElement)
+            elsif positionComparison & 2
+              target.insertAdjacentElement("afterend", draggedElement)
+            end
+
+            event.preventDefault._call
+
+            this.submitTarget.click._call
+          end
+
+          action :dragend do
           end
         end
 
-        action :dragover do |event|
-          event.preventDefault._call
-          return true
-        end
-
-        action :dragenter do |event|
-          event.preventDefault._call
-        end
-
-        action :drop do |event|
-          subject_id = event.dataTransfer.getData("text/plain")
-          draggedElement = this.element.querySelector("[data-reorder-children-action-subject-id='" + subject_id + "']")
-          target = event.target.closest("[draggable=\"true\"]")
-          target_id = target.dataset.reorderChildrenActionSubjectId
-          positionComparison = target.compareDocumentPosition(draggedElement)
-
-          this.subjectIdTarget.value = subject_id
-          this.targetIdTarget.value = target_id
-
-          if positionComparison & 4
-            target.insertAdjacentElement("beforebegin", draggedElement)
-          elsif positionComparison & 2
-            target.insertAdjacentElement("afterend", draggedElement)
+        record SubjectId, id : Int64 | Int32 | Nil do
+          ToHtml.instance_tag_attrs do
+            data_reorder_children_action_subject_id = id
           end
-
-          event.preventDefault._call
-
-          this.submitTarget.click._call
         end
 
-        action :dragend do
-        end
-      end
-
-      record SubjectId, id : Int64 | Int32 | Nil do
-        ToHtml.instance_tag_attrs do
-          data_reorder_children_action_subject_id = id
-        end
-      end
-
-      template do
-        div DragController, DragController.dragstart_action("dragstart"), DragController.drag_action("drag"), DragController.dragover_action("dragover"), DragController.dragenter_action("dragenter"), DragController.drop_action("drop"), DragController.dragend_action("dragend") do
-          action_form(hidden: true).to_html do
-            input DragController.subject_id_target, type: :hidden, name: SUBJECT_ID_FIELD_NAME
-            input DragController.target_id_target, type: :hidden, name: TARGET_ID_FIELD_NAME
-            input DragController.submit_target, type: :submit, name: "submit", value: "submit"
-          end
-          div do
-            association.each do |child|
-              div SubjectId.new(child.id.try(&.value)), draggable: "true" do
-                child_view(child)
+        template do
+          div DragController, DragController.dragstart_action("dragstart"), DragController.drag_action("drag"), DragController.dragover_action("dragover"), DragController.dragenter_action("dragenter"), DragController.drop_action("drop"), DragController.dragend_action("dragend") do
+            action_form(hidden: true).to_html do
+              input DragController.subject_id_target, type: :hidden, name: SUBJECT_ID_FIELD_NAME
+              input DragController.target_id_target, type: :hidden, name: TARGET_ID_FIELD_NAME
+              input DragController.submit_target, type: :submit, name: "submit", value: "submit"
+            end
+            div do
+              association.each do |child|
+                div SubjectId.new(child.id.try(&.value)), draggable: "true" do
+                  child_view(child)
+                end
               end
             end
           end

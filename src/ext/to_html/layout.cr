295fbe23180d3b2db6dced851092f1439bd7858f
Stefan Bilharz <stefan@sbsoftware.de>
2025-09-18 21:38:25 +0200

    CRT-35 Fix style includes

--- src/ext/to_html/layout.cr
+++ src/ext/to_html/layout.cr
@@ -3,8 +3,7 @@ require "../../stimulus_controllers/model_template_refresh_controller"
 
 class ToHtml::Layout
   append_to_head ToHtml::ExternalScript.new("https://unpkg.com/@hotwired/turbo@8.0.4/dist/turbo.es2017-umd.js")
-  append_to_head Orma::ModelAction::GenericModelActionTemplate::Style
-  append_to_head ReorderChildrenAction::Template::Style
+  append_to_head Crumble::Turbo::ActionForm::Style
 
   body_attributes Crumble::Turbo::ModelTemplateRefreshController
 end

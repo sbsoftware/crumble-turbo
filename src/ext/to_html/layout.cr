2bb04018332b5b76c6432301baaf8f861c246f01
Stefan Bilharz <stefan@sbsoftware.de>
2026-09-15 22:44:50 +0200

    Serve TurboJS asset directly
    Ticket: https://tsbp.sbsoftware.de/w/sbsoftware/CRT/6

--- src/ext/to_html/layout.cr
+++ src/ext/to_html/layout.cr
@@ -1,8 +1,9 @@
 require "to_html"
+require "../../crumble/turbo/turbo_asset"
 require "../../stimulus_controllers/model_template_refresh_controller"
 
 class ToHtml::Layout
-  append_to_head ToHtml::ExternalScript.new("https://unpkg.com/@hotwired/turbo@8.0.4/dist/turbo.es2017-umd.js")
+  append_to_head ToHtml::ExternalScript.new(Crumble::Turbo::TurboAsset.uri_path)
   append_to_head Crumble::Turbo::ActionForm::Style
 
   body_attributes Crumble::Turbo::ModelTemplateRefreshController
